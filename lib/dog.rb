fa285d5519118120effbb08eeedbcfc0496f0ec7
Juan Tenezaca <tenj34@gmail.com>
2018-09-23 23:42:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,7 @@ class Dog
         INSERT INTO students (name,breed) VALUES (?,?)
       SQL
       DB[:conn].execute(sql,self.name,self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
   end
 
